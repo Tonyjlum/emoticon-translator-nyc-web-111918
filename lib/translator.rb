f79ff6745927be745e0303354d3a043f4cb80f6e
Tony J. Lum <Tonyjlum@gmail.com>
2018-10-31 20:33:45 -0400

    (＾ｖ＾)

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  data_hsh = YAML.load_file(file_path)
+  emoji_hsh = {"get_meaning" => {}, "get_emoticon" => {}}
+  #get_meaning returns the word
+  #get_emoticon returns the english meaning
+  data_hsh.each do |word, face_array|
+    emoji_hsh["get_emoticon"].merge!(face_array.first => face_array.last)
+    emoji_hsh["get_meaning"].merge!(face_array.last => word)
+  end
+  emoji_hsh
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  emoji_hsh = load_library(file_path)
+  jap_emoji = emoji_hsh["get_emoticon"][emoticon]
+  jap_emoji.nil? ? "Sorry, that emoticon was not found" : jap_emoji
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path,emoticon)
+  emoji_hsh = load_library(file_path)
+  eng_meaning = emoji_hsh["get_meaning"][emoticon]
+  eng_meaning.nil? ? "Sorry, that emoticon was not found" : eng_meaning
 end
